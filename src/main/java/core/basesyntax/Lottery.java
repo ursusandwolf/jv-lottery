3c5a422aaf95920fcaaaa45ad4212976627c0c57
Alex <sumy.ua@gmail.com>
2026-01-18 01:39:27 +0200

    Raw code, did not testet yet So this commit need to check

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+	public Ball getRandomBall() {
+		Color color = Color.valueOf(new ColorSupplier().getRandomColor());
+		int number = new Random().nextInt(101);
+		return new Ball(color, number);
+	}
+
+	public static void main(String[] args) {
+		Lottery lottery = new Lottery();
+		for (int i = 0; i <3 ; i++) {
+			System.out.println(lottery.getRandomBall());
+		}
+	}
+}
