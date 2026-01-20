ce744b4e5b006d33a92540462425eb548484d91d
alex <ursusandwolf@protonmail.com>
2026-01-20 15:28:04 +0200

    private static final MIN MAX RANDOM SUPPLIER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random random = new Random();
+    private static final int MIN = 1;
+    private static final int MAX = 100;
+    private static final Random RANDOM = new Random();
+    private static final ColorSupplier SUPPLIER = new ColorSupplier();
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
@@ -14,7 +17,6 @@ public class Lottery {
 
     public Ball getRandomBall() {
 
-        Color color = new ColorSupplier().getRandomColor();
-        return new Ball(color, random.nextInt(1, 101));
+        return new Ball(SUPPLIER.getRandomColor(), RANDOM.nextInt(MIN, MAX + 1));
     }
 }
