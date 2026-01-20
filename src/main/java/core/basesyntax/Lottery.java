a9f818106dc00c58176671403e9699d522d1e977
alex <ursusandwolf@protonmail.com>
2026-01-20 17:49:22 +0200

    final nonstatic fields

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,20 +3,21 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int INT = 3;
     private static final int MIN = 1;
     private static final int MAX = 100;
-    private static final Random RANDOM = new Random();
-    private static final ColorSupplier SUPPLIER = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier supplier = new ColorSupplier();
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < INT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
 
     public Ball getRandomBall() {
 
-        return new Ball(SUPPLIER.getRandomColor(), RANDOM.nextInt(MIN, MAX + 1));
+        return new Ball(supplier.getRandomColor(), random.nextInt(MIN, MAX + 1));
     }
 }
